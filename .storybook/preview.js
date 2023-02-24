fd1bdaad07f50db583a16149441cfa4a0361b193
Dinh Doan Van Bien <999472+voieducode@users.noreply.github.com>
2023-02-24 22:13:33 +0000

    first tutorial

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import '../src/index.css';
+
+// Configures Stroybook to log the actions (onArchiveTask and onPinTask)
+// in the UI
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
