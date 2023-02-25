ccf7c8d3c9dab1a38d04c20b6a1b7429b5cceecf
Dinh Doan Van Bien <999472+voieducode@users.noreply.github.com>
2023-02-25 23:59:14 +0000

    screens final

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -7,6 +7,13 @@ import store from '../lib/store';
  import { MockedState } from './TaskList.stories';
 import { Provider } from 'react-redux';
 
+import {
+    fireEvent,
+    within,
+    waitFor,
+    waitForElementToBeRemoved
+} from '@storybook/testing-library';
+
 export default {
   component: InboxScreen,
   title: 'InboxScreen',
@@ -27,6 +34,19 @@ export const Default = Template.bind({});
        ),
      ],
    },
+ };
+
+ Default.play = async({canvasElement}) => {
+    const canvas = within(canvasElement);
+    // Waits for the component to transition from the loading state
+    await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
+    // Waits for the component to be updated based on the store
+    await waitFor(async () => {
+        // Simulates pinning the first task
+        await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+        // Simulates pinning the third task
+        await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+    });
  };
 
 export const Error = Template.bind({});
